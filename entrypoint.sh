1248c81b360cc9f6405a236be4f9910c6fe96021
esme518 <esme518@gmail.com>
2020-05-03 23:15:55 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,13 @@
 getent group qbittorrent >/dev/null || addgroup -g ${PGID} qbittorrent
 getent passwd qbittorrent >/dev/null || adduser -S -D -u ${PUID} -G qbittorrent -g qbadmin -s /sbin/nologin qbittorrent
 
+if [ ! -d /torrents/GeoIP ]
+then
+	mkdir /torrents/GeoIP
+fi
+
+wget https://geolite.clash.dev/Country.mmdb -O /torrents/GeoIP/GeoLite2-Country.mmdb
+
 if [ ! -f /config/qBittorrent.conf ]
 then
 	cp /default/qBittorrent.conf /config/qBittorrent.conf
